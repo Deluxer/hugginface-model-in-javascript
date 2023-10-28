62eb1c90fe0b8abfdce57788593ebae7e99e3f72
GeraDeluxer <gerardoangeln@gmail.com>
2023-10-28 11:28:48 -0600

    dataset

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import csvParser from 'csv-parser';
 
 @Injectable()
 export class AppService {
@@ -8,11 +10,48 @@ export class AppService {
 
   async getVector() {
     const transformer = await import('@xenova/transformers')
+    const model = await transformer.pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
 
-    const pipe = await transformer.pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
-    
-    const resp = await pipe('Hola!')
+  
+    const data = [];
+    const spotify_songs = [];
+    const spotify_songs_limit = [];
+    const dataStream = fs.createReadStream('src/dataset/universal_top_spotify_songs.csv')
+      .pipe(csvParser())
+      .on('data', (row) => {
+        spotify_songs.push(row.name);
+      })
+      .on('end', () => {
+        console.log('Lectura de CSV finalizada');
+    });
 
-    return resp;
+    await new Promise((resolve, reject) => {
+      dataStream.on('end', resolve);
+      dataStream.on('error', reject);
+    });
+
+    // spotify_songs.map((name, index) => {
+    //   if (index < 3) {
+    //     spotify_songs_limit.push(name);
+    //   }
+    // });
+
+    for (const name of spotify_songs) {
+      try {
+        const embeddings = await model(name, { pooling: 'mean', normalize: true });
+
+        data.push({
+          name: name,
+          dims: embeddings.dims,
+          embeddings: Object.values(embeddings.data.slice(0, 3)),
+          size: embeddings.size,
+        });
+
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    return data;
   }
 }
